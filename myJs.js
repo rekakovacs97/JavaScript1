5389e9bb1813b9e79e70a40e980ffbcc9d6539f2
rekakovacs97 <reka.kovacs@nmg.ro>
2021-10-11 16:38:10 +0300

    Javascript exercies

---
+++ myJs.js
@@ -0,0 +1,44 @@
+
+document.getElementById("info").addEventListener("click", function() {
+    document.getElementById("info").innerHTML = "Hello Worldsdddsfsdd";
+    document.getElementById("bla").innerHTML = "Blalba";
+    document.getElementById("bla").style.color = "red";
+  });
+
+
+document.getElementById("test").addEventListener("click", function(){
+  document.getElementById("third").innerHTML = "Hello Worldsdddsfsdd";
+});
+  
+
+
+//function myFunction(){
+ // document.getElementById("first").classList.add("first--test'");
+//}
+
+
+
+ // document.getElementById("info").addEventListener("click", function() {
+   // document.getElementsByClassName("first").addClass("first--test");
+  //});
+
+//}
+
+//document.getElementById("MyElement").classList.add('MyClass');
+
+
+
+//function myFunction() {
+   // document.getElementById("info").addEventListener("click", function() {
+  //      document.getElementById("first").addId("first--test");
+ //     });
+  //}
+
+ // $(".something").click(function(){
+ //   $(this).addClass("style");
+//});
+
+
+//$( document ).ready(function() {
+  //   $( ".first" ).addClass( "first--test" );
+//});
